f1f46abb49f9f723c7af52636af5f14f86b93cda
QuarkPixel <m2462864403@gmail.com>
2024-06-03 09:47:57 +0800

    Highlight when flap

--- src/libs.rs
+++ src/libs.rs
@@ -1,5 +1,6 @@
 use bracket_lib::prelude::BTerm;
 use crate::palette;
+use crate::palette::Cor;
 
 #[allow(dead_code)]
 pub fn print(ctx: &mut BTerm, x: i32, y: i32, text: &str) {
@@ -10,7 +11,7 @@ pub fn print_c(ctx: &mut BTerm, y: i32, text: &str) {
     ctx.print_color_centered(y, palette::TEXT, palette::TEXT_BG, text);
 }
 
-pub fn blend_colors(color1: (u8, u8, u8), color2: (u8, u8, u8), t: f32) -> (u8, u8, u8) {
+pub fn blend_colors(color1: Cor, color2: Cor, t: f32) -> (u8, u8, u8) {
     // 确保t在0.0到1.0之间
     let t = t.clamp(0.0, 1.0);
 
